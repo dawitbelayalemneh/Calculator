a15bdba018886f1936154d9e74fdda9f65bf892e
Dawit Belay <dawitbelay27@gmail.com>
2024-11-18 23:04:29 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// CALCULATORPRORAM
+
+const display = document.getElementById("display");
+
+function appendToDisplay(input){
+    display.value += input;
+}
+
+function clearDisplay(){
+    display.value = "0" ;
+}
+
+function calculate(){
+    try{
+        display.value = eval(display.value);
+    }
+    catch(error){
+        display.value = "ERROR"
+    }
+}
